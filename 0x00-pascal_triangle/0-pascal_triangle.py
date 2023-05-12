0ba5cce683284fffeef69f4961f046568be4b318
Otswag <emmanuel.amadi68@gmail.com>
2023-05-12 05:02:28 -0700

    Debuggiing Pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,9 +7,9 @@ Module of Pascal's triangle of n
 def pascal_triangle(n):
     if n <= 0:
         return []
-    triangle = []
-    for i in range(n):
-        row = []
+    triangle = [1]
+    for i in range(1, n):
+        row = [1]
         for j in range(i + 1):
             if j == 0 or j == i:
                 row.append(1)
