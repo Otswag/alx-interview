5966afedfb473aba47fa70fb859b8c427937383f
Otswag <emmanuel.amadi68@gmail.com>
2023-05-12 14:32:27 -0700

    getting done with log parsing project debugging

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,15 +2,18 @@
 """
 Reads stdin line by line and computes metrics.
 
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Input format: <IP Address> - [<date>] "GET/projects/260
+HTTP/1.1" <status code> <file size>
 (if the format is not this one, the line must be skipped)
 
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
+After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+  statistics from the beginning:
 - Total file size: File size: <total size>
   where <total size> is the sum of all previous <file size>
 - Number of lines by status code:
   possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-  if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+  if a status code doesn’t appear or is not an integer, don’t print anything
+  for this status code
   format: <status code>: <number>
   status codes should be printed in ascending order
 
@@ -26,7 +29,8 @@ def print_stats(total_size, status_codes):
 
     Args:
         total_size (int): The total file size.
-        status_codes (dict): A dictionary that maps status codes to their counts.
+        status_codes (dict): A dictionary that maps
+        status codes to their counts.
     """
     print("File size: {:d}".format(total_size))
     for status_code in sorted(status_codes.keys()):
@@ -36,7 +40,8 @@ def print_stats(total_size, status_codes):
 
 def main():
     total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0,
+                    500: 0}
     i = 0
 
     try:
@@ -51,7 +56,7 @@ def main():
                 if status_code.isdigit() and int(status_code) in status_codes:
                     status_codes[int(status_code)] += 1
                     total_size += int(file_size)
-                else:
+            else:
                 continue
     except KeyboardInterrupt:
         pass
