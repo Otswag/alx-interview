dc61d0dae629cbc690c1542bc7f6e367d1371435
Otswag <emmanuel.amadi68@gmail.com>
2023-05-11 17:10:19 -0700

    Pascal triangle project

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
+
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
@@ -8,4 +9,5 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
+
     return triangle
