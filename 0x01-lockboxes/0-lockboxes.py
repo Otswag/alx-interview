d4523b7420b532d37e8ff1ea8219c7302240b354
Otswag <emmanuel.amadi68@gmail.com>
2023-05-12 05:41:30 -0700

    Project code to unlock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-"""
-Determines if all the boxes can be opened
-"""
+"""Module that contains a function that determines if all boxes can be unlocked"""
 
 
 def canUnlockAll(boxes):
     """
-    Returns True if all boxes can be opened, else False
+    Determines if all boxes can be unlocked.
+    Args:
+        boxes (list): A list of lists containing the keys.
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
     """
-    if not boxes:
-        return False
-
-    # Create a set of the box indices, and mark box 0 as opened
-    box_indices = set(range(len(boxes)))
-    opened_boxes = {0}
-
-    # Iterate through the list of boxes
-    while box_indices:
-        # Get the set of keys in the opened boxes
-        keys = set(key for box in opened_boxes for key in boxes[box])
-
-        # Find the set of boxes that can be opened with the keys
-        openable_boxes = box_indices.intersection(keys)
-
-        # If there are no more openable boxes, break out of the loop
-        if not openable_boxes:
-            break
-
-        # Mark the openable boxes as opened, and remove them
-
-        # from the set of unopened boxes
-        opened_boxes.update(openable_boxes)
-        box_indices.difference_update(openable_boxes)
-
-    # Return True if all boxes have been opened, else False
-    return not box_indices
+    n = len(boxes)
+    unlocked_boxes = [False] * n
+    unlocked_boxes[0] = True
+    keys = boxes[0]
+    for key in keys:
+        if key < n:
+            unlocked_boxes[key] = True
+            keys.extend(boxes[key])
+    return all(unlocked_boxes)
