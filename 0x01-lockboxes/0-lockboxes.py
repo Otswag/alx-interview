e4d878fada4b1475f0fbed430814b9766a5641fb
Otswag <emmanuel.amadi68@gmail.com>
2023-05-12 06:17:52 -0700

    Debugging lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-"""Module that contains a function that determines if all boxes can be unlocked"""
+"""
+Module that determines if all the boxes can be opened.
+"""
 
 
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be unlocked.
+    Determines if all the boxes can be opened.
+
     Args:
-        boxes (list): A list of lists containing the keys.
+        boxes: A list of lists containing the keys to other boxes.
+
     Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
+        True if all boxes can be opened, False otherwise.
     """
-    n = len(boxes)
-    unlocked_boxes = [False] * n
-    unlocked_boxes[0] = True
-    keys = boxes[0]
-    for key in keys:
-        if key < n:
-            unlocked_boxes[key] = True
-            keys.extend(boxes[key])
-    return all(unlocked_boxes)
+
+    # Create a set to keep track of all the boxes that have been opened.
+    opened_boxes = set()
+    # Add the first box (index 0) to the set of opened boxes.
+    opened_boxes.add(0)
+    # Create a queue to keep track of boxes that have not yet been opened.
+    unopened_boxes = boxes[0][:]
+    # While there are still unopened boxes:
+    while unopened_boxes:
+        # Pop a box from the front of the queue.
+        box = unopened_boxes.pop(0)
+        # If we have not already opened this box:
+        if box not in opened_boxes:
+            # Add it to the set of opened boxes.
+            opened_boxes.add(box)
+            # Add all the keys in this box to the queue of unopened boxes.
+            unopened_boxes.extend(boxes[box])
+    # If the set of opened boxes contains all the boxes, return True.
+    if len(opened_boxes) == len(boxes):
+        return True
+    # Otherwise, return False.
+    return False
