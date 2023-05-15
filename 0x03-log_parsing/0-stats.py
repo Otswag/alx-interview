2fee077900b39c9ddd0e48db42882e5cb727eb97
otswag <emmanuel.amadi68@gmail.com>
2023-05-15 17:41:18 +0000

    Debugging log parsing project

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,45 @@
 #!/usr/bin/python3
 
-"""
-Script that reads a log file line by line and computes metrics
-"""
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
-import re
 
-# Define regular expression pattern to match log entries
-LOG_PATTERN = r'(\d+\.\d+\.\d+\.\d+) - - \[(.*?)\] "(.*?)" (\d+) (\d+)'
 
-# Create dictionary to store status code counts
-STATUS_CODES =
-{'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0,
- '500': 0}
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-# Initialize total size counter
-TOTAL_SIZE = 0
 
-# Open log file and read each line
-with open('access.log') as log_file:
-    for line in log_file:
-        # Match log entry to pattern
-        match = re.match(LOG_PATTERN, line)
-        if match:
-            # Extract status code and request size from log entry
-            status_code = match.group(4)
-            size = int(match.group(5))
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-            # Increment status code count and total size
-            if status_code in STATUS_CODES:
-                STATUS_CODES[status_code] += 1
-            TOTAL_SIZE += size
+count = 0
+size = 0
 
-            # Print status code counts every 10 entries
-            if len(STATUS_CODES) % 10 == 0:
-                print("Status code counts:", STATUS_CODES)
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-# Print final status code counts and total size
-print("Status code counts:", STATUS_CODES)
-print("Total size:", TOTAL_SIZE)
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
+        except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
