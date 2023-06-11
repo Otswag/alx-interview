ba7a4d1c90504bec22ef0c6f9f95fa5b080dac56
Otswag <106761383+Otswag@users.noreply.github.com>
2023-06-12 00:31:13 +0100

    Debugging utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+
 """UTF-8 Validation"""
 
-
 def validUTF8(data):
+
     """Determines if a given data set
+
     represents a valid utf-8 encoding
+
     """
+
     number_bytes = 0
 
     mask_1 = 1 << 7
+
     mask_2 = 1 << 6
 
     for i in data:
@@ -18,22 +23,29 @@ def validUTF8(data):
         if number_bytes == 0:
 
             while mask_byte & i:
+
                 number_bytes += 1
+
                 mask_byte = mask_byte >> 1
 
             if number_bytes == 0:
+
                 continue
 
             if number_bytes == 1 or number_bytes > 4:
+
                 return False
 
-            else:
+        else:
+
             if not (i & mask_1 and not (i & mask_2)):
+
                 return False
 
         number_bytes -= 1
 
     if number_bytes == 0:
+
         return True
 
     return False
