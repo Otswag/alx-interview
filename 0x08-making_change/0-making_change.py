e888ab88080c4141ac40ecfc6b6b898ab6e0669b
Otswag <106761383+Otswag@users.noreply.github.com>
2023-06-14 23:58:30 +0100

    Debugging making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,10 +9,21 @@ def makeChange(coins, total):
     Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total
     """
+    
     if total <= 0:
+
         return 0
-    dp = [0] + [float("inf")] * (total)
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[-1] if dp[-1] != float("inf") else -1
+
+    dp = [0] + [float("inf")] * total
+
+    coin_set = set(coins)
+
+    for i in range(1, total + 1):
+
+        for coin in sorted(coin_set):
+
+            if i >= coin:
+
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float("inf") else -1
